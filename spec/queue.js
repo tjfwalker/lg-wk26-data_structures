63260b38612a5e48baef7781fc38c58d1eb3f8ff
TJ Walker <tjfwalker+git@gmail.com>
2017-01-10 09:15:50 -0800

    Create base for Queue implementation and tests

---
+++ spec/queue.js
@@ -0,0 +1,10 @@
+import chai, { expect } from 'chai'
+import Queue from '../src/queue'
+
+describe('Queue', function () {
+  'use strict'
+
+  it('exists', function () {
+    expect(Queue).to.be.a('function')
+  })
+})
---
+++ src/queue.js
@@ -0,0 +1,7 @@
+'use strict'
+
+export default class Queue {
+  constructor() {
+    this.data = []
+  }
+}
