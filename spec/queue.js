18db4cdc630feb4a1fcaf9dca2ad560921057a24
TJ Walker <tjfwalker+git@gmail.com>
2017-01-10 09:59:33 -0800

    Create unit test for queue.length()

--- spec/queue.js
+++ spec/queue.js
@@ -7,4 +7,30 @@ describe('Queue', function () {
   it('exists', function () {
     expect(Queue).to.be.a('function')
   })
+
+  beforeEach( function () {
+    this.queue = new Queue
+  })
+
+  context('length()', function () {
+    it('exists', function () {
+      expect(this.queue.length).to.be.a( 'function' )
+    })
+
+    it('returns the number of elements in the queue', function () {
+      expect(this.queue.length()).to.equal( 0 )
+
+      this.queue.enqueue( 'foo' )
+      expect(this.queue.length()).to.equal( 1 )
+
+      this.queue.enqueue( 'bar' )
+      expect(this.queue.length()).to.equal( 2 )
+
+      this.queue.dequeue()
+      expect(this.queue.length()).to.equal( 1 )
+
+      this.queue.enqueue( 'baz' )
+      expect(this.queue.length()).to.equal( 2 )
+    })
+  })
 })
